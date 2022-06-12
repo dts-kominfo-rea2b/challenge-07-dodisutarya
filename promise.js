2fe1daabbc0cb4020e0a234d257d105c33c42f75
Dodi Sutarya <9284479+dodisutarya@users.noreply.github.com>
2022-06-12 23:08:01 +0700

    promise

--- promise.js
+++ promise.js
@@ -1,8 +1,34 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
 
+//---------------------------------
+
+const countData = (data, findData) => {
+  return new Promise((resolve, reject) => {
+    if (findData == undefined) {
+      reject("Data tidak ditemukan!")
+    } else {
+      let result = 0;
+      for (let i = 0; i < data.length; i++) {
+        if (findData == data[i]['hasil']) {
+          result++;
+        }
+      }
+      resolve(result);
+    }
+  })
+}
+
+const promiseOutput = async (findData) => {
+  const hitungTheaterIXX = await promiseTheaterIXX()
+    .then((hasilSetelahMenonton) => countData(hasilSetelahMenonton, findData))
+  const hitungTheaterVGC = await promiseTheaterVGC()
+    .then((hasilSetelahMenonton) => countData(hasilSetelahMenonton, findData))
+  return hitungTheaterIXX + hitungTheaterVGC
+}
+
+//--------------------------------------
 module.exports = {
   promiseOutput,
 };
